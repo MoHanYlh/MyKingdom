e2d7985e94f5989b4ad5796953e1cf3790af8461
MoHanYlh <mohanylh@aliyun.com>
2017-11-21 17:18:35 +0800

    MoHan - office
    login finished

--- src/main/java/io/imking/core/controller/admin/LoginController.java
+++ src/main/java/io/imking/core/controller/admin/LoginController.java
@@ -1,17 +1,14 @@
 package io.imking.core.controller.admin;
 
-import static org.mockito.Matchers.anyMap;
-
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.sql.DataSource;
 
-import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -40,40 +37,48 @@ import io.imking.core.vo.RestResponse;
 @RequestMapping("/admin")
 public class LoginController {
 
-    private static final Logger logger = Logger.getLogger(LoginController.class);
-
     @Autowired
     private UserService userService;
-
+    
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    
     @RequestMapping(value = "/login", method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     public RestResponse login(HttpServletRequest request, HttpServletResponse response,
             @RequestBody Map<String, Object> params) {
+        
+        stringRedisTemplate.opsForValue().set("aaa", "111");
 
         String username = params != null && params.containsKey("username") ? (String) params.get("username") : null;
         String password = params != null && params.containsKey("password") ? (String) params.get("password") : null;
+        String countString = stringRedisTemplate.opsForValue().get(username);
         RestResponse result = null;
         HttpSession session = request.getSession();
         
         if(!StringUtils.isEmpty(username) && !StringUtils.isEmpty(password)) {
             User user = userService.login(username,MD5Tools.MD5(password));
-            System.out.println(MD5Tools.MD5(password));
-            System.out.println(user);
             if(user != null && user.getStatus() == (byte)1) {
                 session.setAttribute("admin", user);
                 Map<String, Object> responseData = new HashMap<String, Object>();
                 responseData.put("id", user.getId());
                 responseData.put("username", user.getUserName());
                 result = RestResponse.success(responseData);
+                stringRedisTemplate.opsForValue().set(username, String.valueOf(0));
             } else if(user != null && user.getStatus() == (byte)2) {
                 result = RestResponse.message(403, "用户被禁用");
             } else {
                 result = RestResponse.message(403, "用户名或密码错误");
-                /**
-                 * 错误计数
-                 */
+                // 错误计数
+                if(countString != null) {
+                    int count = Integer.parseInt(countString);
+                    count++;
+                    stringRedisTemplate.opsForValue().set(username, String.valueOf(count));
+                }else {
+                    stringRedisTemplate.opsForValue().set(username, String.valueOf(1));
+                }
             }
         } else {
             result = RestResponse.failure("参数错误");
@@ -87,8 +92,16 @@ public class LoginController {
     @ResponseBody
     public RestResponse loginCheck(HttpServletRequest request, HttpServletResponse response,
             @RequestBody Map<String, Object> params) {
-
-        return null;
+        
+        String username = params != null && params.containsKey("username") ? (String) params.get("username") : null;
+        RestResponse result = null;
+        String countString = stringRedisTemplate.opsForValue().get(username);
+        if(countString != null && Integer.parseInt(countString) >= 3) {
+            result = RestResponse.message(403, "请输入验证码");
+        }else {
+            result = RestResponse.message(200, "无需验证");
+        }
+        return result;
     }
 
     @RequestMapping("/loginSuccess")
--- src/main/resources/static/admin/login/login.js
+++ src/main/resources/static/admin/login/login.js
@@ -1,15 +1,13 @@
-$(function() {
-	createCode();
-})
-/**
- * alert()可用toastr替换。
- */
 var code;
+var show = 0;
 // 去掉 O o 0 i I l L 难以区分的字母
 var codeArray = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e',
 		'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v',
 		'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K',
 		'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+/**
+ * 生成验证码
+ */
 function createCode() {
 	code = "";
 	var codeLength = 6;
@@ -19,19 +17,55 @@ function createCode() {
 	}
 	$("#checkCode").html(code);
 }
-
+/**
+ * 显示验证码输入框
+ */
+function showValidate(){
+	$('#pwdDiv').addClass('line');
+	$('#codeDiv').css('display', '');
+	$('#code').attr('required', '');
+	show = 1;
+	createCode();
+}
+/**
+ * 检查是否需要输入验证码
+ */
 function loginCheck() {
-	var validateCode = code.toUpperCase();
-	var inputCode = $('#code').val().toUpperCase();
-	console.log(validateCode);
-	console.log(inputCode)
-	if (validateCode == inputCode) {
-		login();
-	} else {
-		alert("验证码错误");
-		createCode();
+	if(show == 0){
+		$.ajax({
+			type : "POST",
+			url : "/admin/loginCheck",
+			data : JSON.stringify({
+				'username' : $('#username').val()
+			}),
+			success : function(rs) {
+				if(rs && rs.status == 200){
+					login();
+				}else{
+					alert(rs.message);
+					showValidate();
+				}
+			},
+			error : function() {
+				alert("网络错误");
+			},
+			dataType : "json",
+			contentType : "application/json"
+		});
+	}else{
+		var validateCode = code.toUpperCase();
+		var inputCode = $('#code').val().toUpperCase();
+		if (validateCode == inputCode) {
+			login();
+		} else {
+			alert("验证码错误");
+			createCode();
+		}
 	}
 }
+/**
+ * 登录
+ */
 function login() {
 	$.ajax({
 		type : "POST",
@@ -42,7 +76,8 @@ function login() {
 		}),
 		success : function(rs) {
 			if (rs && rs.status == 200) {
-				alert("登录成功")
+				alert("登录成功");
+				show = 0;
 				var data = rs.data;
 				localStorage.setItem("rspm.local", JSON.stringify(data));
 			} else {
